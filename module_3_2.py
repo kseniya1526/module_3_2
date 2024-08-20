b24084401ae41cf14c144012ae178e6b4dc3b8d9
kseniya1526 <kseniak505@gmail.com>
2024-08-20 21:36:34 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,20 @@
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    if not all(['@' in recipient,'@' in sender,
+                       recipient.endswith('.com') or
+                       recipient.endswith('.ru') or
+                       recipient.endswith('. net'),
+                       sender.endswith('.com') or
+                       sender.endswith('.ru') or
+                       sender.endswith('.net')]):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+
+send_email('Это сообщение для проверки связи', 'kseniak505@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
